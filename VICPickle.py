ac07181419fcfd974053004fed873e8c73a9a056
Githika Tondapu <gtondapu@usra.edu>
2017-03-28 16:30:52 -0500

    Delete VICPickle.py

--- VICPickle.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import csv
-import binascii
-mydict = {'s3_base': 'https://bucket.servirglobal.net.s3.amazonaws.com//regions/himalaya/data/eodata/vic/grid/', 'extract_Folder': 'E:\Temp\VIC_Extract\\', 'mosaicDS': 'E:\SERVIR\Data\himalaya\VIC\\', 'finalTranslateFolder': 'E:\servir\data\himalaya\VIC\VIC_Data', 'logFileDir': 'D:\Logs\ETL_Logs\VIC','current_AdminDirURL':'http://localhost:6080/arcgis/admin','current_Username':'YOUR_USERNAME','current_Password':'YOUR_PASSWORD','current_FolderName':'Himalaya','HKH_VIC':'MapServer'}
-output = open('config.pkl', 'wb')
-pickle.dump(mydict, output)
-output.close()
